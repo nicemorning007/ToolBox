3cf423988f556288b9d3191ff0ef3f0e6014ee54
Nicemorning <oypebook@outlook.com>
2018-03-11 17:57:48 +0800

    完成指南针

--- app/src/main/java/cn/nicemorning/toolbox/MainActivity.java
+++ app/src/main/java/cn/nicemorning/toolbox/MainActivity.java
@@ -1,13 +1,20 @@
 package cn.nicemorning.toolbox;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import cn.nicemorning.toolbox.view.CircleMenuLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements SensorEventListener {
 
     private CircleMenuLayout mCircleMenuLayout;
     private String[] mItemTexts = new String[]{"放大镜", "工具尺", "分贝测试仪", "手电筒",
@@ -16,11 +23,19 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.home_mbank_2_normal, R.drawable.home_mbank_3_normal,
             R.drawable.home_mbank_4_normal, R.drawable.home_mbank_5_normal,
             R.drawable.home_mbank_6_normal};
+    private ImageView image_znz;
+    private float angle = 0f;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image_znz = findViewById(R.id.iv_znz);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
         mCircleMenuLayout = findViewById(R.id.id_menulayout);
         mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);
         mCircleMenuLayout.setOnMenuItemClickListener(
@@ -62,5 +77,38 @@ public class MainActivity extends AppCompatActivity {
                                 Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+            float zangle = event.values[0];
+            RotateAnimation rotateAnimation = new RotateAnimation(angle, -zangle,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+            rotateAnimation.setDuration(200);
+            rotateAnimation.setFillAfter(true);
+            image_znz.startAnimation(rotateAnimation);
+            angle = -zangle;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor((Sensor.TYPE_ORIENTATION)),
+                SensorManager.SENSOR_DELAY_GAME);
     }
 }
